571ff1ff4be3e766731c694c62bf51f5961ed628
Sainath <sainath@analyticsquad4.com>
2023-10-05 21:17:45 +0530

    insta

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ export default function Home() {
       <div className="flex justify-center pb-6 text-2xl">Notes</div>
       <div className="flex flex-1 justify-center items-center h-screen w-full">
       <GoogleAuth/>
+      <a href="instagram://user?username=sainath__48">Link to Instagram Profile</a> 
       </div>
     </main>
   )
